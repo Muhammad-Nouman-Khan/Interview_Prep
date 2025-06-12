a746ed5694172877aed0b81adcdf2646545d8e62
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-12 16:32:49 +0500

    Sign Up and Sign In

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import SignUp from "./pages/SignUp";
+import { Routes, Route, Navigate } from "react-router";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="min-h-screen" data-theme="forest">
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,140 @@
+import { Link } from "react-router";
+
+const SignIn = () => {
+  return (
+    <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8">
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/* Left Side */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* LOGO */}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <img src="logo.svg" alt="" />
+
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+              TalkHire
+            </span>
+          </div>
+
+          <div className="w-full mt-4">
+            <form>
+              <div className="space-y-3">
+                {/* FULLNAME */}
+                {/* <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Full Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Nouman Khan"
+                    className="input input-bordered w-full"
+                    required
+                  />
+                </div> */}
+
+                {/* Email */}
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="someone@example.com"
+                    className="input input-bordered w-full"
+                    required
+                  />
+                </div>
+                {/* Password */}
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="******"
+                    className="input input-bordered w-full"
+                    required
+                  />
+                  <p className="text-xs opacity-70 mt-1">
+                    Password must be at least 6 characters long
+                  </p>
+                </div>
+                {/* Profile Picture */}
+                {/* <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Profile picture</span>
+                  </label>
+                  <input
+                    type="file"
+                    className="file-input file-input-bordered w-full "
+                  />
+                </div> */}
+                {/* Resume */}
+                {/* <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Resume</span>
+                  </label>
+                  <input
+                    type="file"
+                    className="file-input file-input-bordered w-full "
+                  />
+                </div> */}
+                {/* Terms and Privacy Policy */}
+                {/* <div className="form-control">
+                  <label className="label cursor-pointer justify-start gap-2">
+                    <input
+                      type="checkbox"
+                      className="checkbox checkbox-sm"
+                      required
+                    />
+                    <span className="text-xs leading-tight">
+                      I agree to the{" "}
+                      <span className="text-primary hover:underline">
+                        terms of service
+                      </span>{" "}
+                      and{" "}
+                      <span className="text-primary hover:underline">
+                        privacy policy
+                      </span>
+                    </span>
+                  </label>
+                </div> */}
+              </div>
+              <button className="btn mt-4 btn-primary w-full" type="submit">
+                Sign In
+              </button>
+              <div className="text-center mt-4">
+                <p className="text-sm">
+                  Don't have an account?{" "}
+                  <Link
+                    to="/signup"
+                    className="text-primary hover:underline cursor-pointer"
+                  >
+                    Sign up
+                  </Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+        {/* Right Side */}
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w-md p-8">
+            <div className="relative aspect-square max-w-sm mx-auto">
+              <img
+                src="/interview.png"
+                alt="Language connection illustration"
+                className="w-full h-full"
+              />
+            </div>
+
+            <div className="text-center space-y-3 mt-6">
+              <h3 className="text-2xl ">Practice job interviews with AI</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
