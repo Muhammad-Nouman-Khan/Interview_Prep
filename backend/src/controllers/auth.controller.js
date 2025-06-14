6225774a8741b5230d55f2096e2508c82ef8c160
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-14 12:21:27 +0500

    Add login and logout functionality to auth controller; update routes and integrate React Query in frontend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+
 export const signup = async (req, res) => {
   const { email, password, fullName, profilePic, resume } = req.body;
   try {
@@ -54,4 +55,52 @@ export const signup = async (req, res) => {
       message: "Internal server error",
     });
   }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // only the server can access the cookie
+      secure: process.env.NODE_ENV === "production", // only send the cookie in https
+      sameSite: "strict", // prevent csrf attacks
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      user,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.clearCookie("jwt");
+  res.status(200).json({ message: "Logged out successfully" });
 };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SignUp from "./pages/SignUp";
-import { Routes, Route, Navigate } from "react-router";
+import { Routes, Route } from "react-router";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 
